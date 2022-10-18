b73c79022e95ce5d46d1099c1b359751ec3d1268
2021 <458761603@qq.com>
2022-10-18 12:52:02 +0000

    fix(example): fix memory

--- examples/memory/src/main.rs
+++ examples/memory/src/main.rs
@@ -1,44 +1,66 @@
 use std::path::Path;
 
-use wasmedge_asyncify::{types::WasmVal, *};
+use wasmedge_asyncify::{
+    error::CoreError,
+    module::{AsyncInstance, AsyncInstanceRef},
+    store::Store,
+    types::{ValType, WasmVal},
+    *,
+};
 
-fn async_host_to_uppercase(linker: &mut AsyncLinker, args: Vec<types::WasmVal>) -> ResultFuture {
-    Box::new(async move {
-        if let (Some(WasmVal::I32(offset)), Some(WasmVal::I32(len))) = (args.get(0), args.get(1)) {
-            let bytes = linker.get_memory_mut("memory", *offset as usize, *len as usize)?;
-            if let Ok(s) = std::str::from_utf8_mut(bytes) {
-                let new_s = s.to_uppercase();
-                bytes.clone_from_slice(new_s.as_bytes());
-                Ok(vec![WasmVal::I32(*len)])
-            } else {
-                Ok(vec![WasmVal::I32(-1)])
-            }
+fn sync_sleep<'a>(
+    _inst: &'a mut AsyncInstanceRef,
+    mem: &'a mut Memory,
+    _data: &'a mut i32,
+    args: Vec<types::WasmVal>,
+) -> Result<Vec<WasmVal>, CoreError> {
+    if let (Some(WasmVal::I32(offset)), Some(WasmVal::I32(len))) = (args.get(0), args.get(1)) {
+        let bytes = mem.data_pointer_mut(*offset as usize, *len as usize);
+        if bytes.is_none() {
+            return Ok(vec![WasmVal::I32(-1)]);
+        }
+        let bytes = bytes.unwrap();
+        if let Ok(s) = std::str::from_utf8_mut(bytes) {
+            let new_s = s.to_uppercase();
+            bytes.clone_from_slice(new_s.as_bytes());
+            Ok(vec![WasmVal::I32(*len)])
         } else {
             Ok(vec![WasmVal::I32(-1)])
         }
-    })
+    } else {
+        Ok(vec![WasmVal::I32(-1)])
+    }
 }
 
 #[tokio::main]
 async fn main() {
-    let config = crate::Config::create().unwrap();
+    let config = Some(Config::create().unwrap());
+    let executor = Executor::create(&config).unwrap();
+    let mut store = Store::create().unwrap();
+    let mut wasi_import = wasmedge_asyncify::wasi::AsyncWasiImport::new().unwrap();
+    wasi_import.push_env("a", "1");
+    wasi_import.push_env("b", "2");
 
-    let mut builder = crate::AsyncLinkerBuilder::new(&Some(config)).unwrap();
+    let mut host_import = ImportModule::create("host", 1).unwrap();
+    host_import
+        .add_sync_func(
+            "to_uppercase",
+            (vec![ValType::I32, ValType::I32], vec![ValType::I32]),
+            sync_sleep,
+        )
+        .unwrap();
+    store
+        .register_import_object(&executor, &mut wasi_import)
+        .unwrap();
+    store
+        .register_import_object(&executor, &mut host_import)
+        .unwrap();
+
+    let loader = Loader::create(&config).unwrap();
 
     // create a wasi module
-    builder.create_wasi(&[], &["b=1", "a=1"], &[]).unwrap();
 
     // create a async import module
-    builder
-        .create_import_object("host", |b| {
-            b.add_async_func(
-                "to_uppercase",
-                (vec![ValType::I32, ValType::I32], vec![ValType::I32]),
-                async_host_to_uppercase,
-            )?;
-            Ok(())
-        })
-        .unwrap();
 
     // read wasm
     let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
@@ -47,11 +69,12 @@ async fn main() {
     let wasm = std::fs::read(wasm_path).unwrap();
 
     // load wasm from bytes
-    let module = builder.load_wasm(&wasm).unwrap();
+    let module = loader.load_async_module_from_bytes(&wasm).unwrap();
 
     // instance wasm
-    let mut inst = builder.instance(&module).unwrap();
+    let mut inst = AsyncInstance::instance(executor, &mut store, &module).unwrap();
 
     // call _start function
-    inst.call("_start", vec![]).await.unwrap();
+    println!("call _start");
+    inst.call("_start", vec![]).unwrap().await.unwrap();
 }
