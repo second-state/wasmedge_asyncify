99ce20b1054992a79de1e74bd1c4294b763ce2e3
2021 <458761603@qq.com>
2022-11-04 10:02:06 +0000

    example: rename `memory` to `memory_sync_host`

--- examples/memory_sync_host/src/main.rs
+++ examples/memory_sync_host/src/main.rs
@@ -8,7 +8,15 @@ use wasmedge_asyncify::{
     *,
 };
 
-fn sync_sleep<'a>(
+fn load_wasm_bytes(path: &str) -> Vec<u8> {
+    let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+    let wasm_path = Path::new(&manifest_dir).join(&format!("../../{}", path));
+    println!("load wasm from {:?}", wasm_path);
+    let wasm = std::fs::read(wasm_path).unwrap();
+    wasm
+}
+
+fn to_uppercase<'a>(
     _inst: &'a mut AsyncInstanceRef,
     mem: &'a mut Memory,
     _data: &'a mut i32,
@@ -36,19 +44,21 @@ fn sync_sleep<'a>(
 async fn main() {
     let config = Some(Config::create().unwrap());
     let executor = Executor::create(&config).unwrap();
-    let mut store = Store::create().unwrap();
-    let mut wasi_import = wasmedge_asyncify::wasi::AsyncWasiImport::new().unwrap();
-    wasi_import.push_env("a", "1");
-    wasi_import.push_env("b", "2");
 
+    // create a wasi module
+    let mut wasi_import = wasmedge_asyncify::wasi::AsyncWasiImport::new().unwrap();
+
+    // create a async import module
     let mut host_import = ImportModule::create("host", 1).unwrap();
     host_import
         .add_sync_func(
             "to_uppercase",
             (vec![ValType::I32, ValType::I32], vec![ValType::I32]),
-            sync_sleep,
+            to_uppercase,
         )
         .unwrap();
+
+    let mut store = Store::create().unwrap();
     store
         .register_import_object(&executor, &mut wasi_import)
         .unwrap();
@@ -58,15 +68,8 @@ async fn main() {
 
     let loader = Loader::create(&config).unwrap();
 
-    // create a wasi module
-
-    // create a async import module
-
     // read wasm
-    let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
-    let wasm_path = Path::new(&manifest_dir).join("../../wasm/memory.wasm");
-    println!("load wasm from {:?}", wasm_path);
-    let wasm = std::fs::read(wasm_path).unwrap();
+    let wasm = load_wasm_bytes("wasm/memory.wasm");
 
     // load wasm from bytes
     let module = loader.load_async_module_from_bytes(&wasm).unwrap();
