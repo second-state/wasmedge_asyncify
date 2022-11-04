a1eeb972b088f9784b5a5deb88aa3d51ae596d80
2021 <458761603@qq.com>
2022-11-04 09:46:34 +0000

    example: rename `hello` to `hello_async_host`

--- examples/hello_async_host/README.md
+++ examples/hello_async_host/README.md
@@ -1,7 +1,7 @@
-## Hello
+## Hello Async Host Function
 This is a simple demo for wasmedge async function.
 
 You can run it with `cargo run`:
 ```
-cargo run --package hello
+cargo run --package hello_async_host
 ```
--- examples/hello_async_host/src/main.rs
+++ examples/hello_async_host/src/main.rs
@@ -6,6 +6,14 @@ use wasmedge_asyncify::{
     types, Config, Executor, ImportModule, Loader, Memory,
 };
 
+fn load_wasm_bytes(path: &str) -> Vec<u8> {
+    let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+    let wasm_path = Path::new(&manifest_dir).join(&format!("../../{}", path));
+    println!("load wasm from {:?}", wasm_path);
+    let wasm = std::fs::read(wasm_path).unwrap();
+    wasm
+}
+
 fn async_sleep<'a>(
     _inst: &'a mut AsyncInstanceRef,
     _mem: &'a mut Memory,
@@ -26,14 +34,18 @@ async fn main() {
     let config = Some(Config::create().unwrap());
     let executor = Executor::create(&config).unwrap();
     let mut store = Store::create().unwrap();
+
+    // create a wasi module
     let mut wasi_import = wasmedge_asyncify::wasi::AsyncWasiImport::new().unwrap();
     wasi_import.push_env("a", "1");
     wasi_import.push_env("b", "2");
 
+    // create a async import module
     let mut host_import = ImportModule::create("host", 1).unwrap();
     host_import
         .add_async_func("async_sleep", (vec![], vec![]), async_sleep)
         .unwrap();
+
     store
         .register_import_object(&executor, &mut wasi_import)
         .unwrap();
@@ -43,15 +55,8 @@ async fn main() {
 
     let loader = Loader::create(&config).unwrap();
 
-    // create a wasi module
-
-    // create a async import module
-
     // read wasm
-    let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
-    let wasm_path = Path::new(&manifest_dir).join("../../wasm/hello.wasm");
-    println!("load wasm from {:?}", wasm_path);
-    let wasm = std::fs::read(wasm_path).unwrap();
+    let wasm = load_wasm_bytes("wasm/hello.wasm");
 
     // load wasm from bytes
     let module = loader.load_async_module_from_bytes(&wasm).unwrap();
